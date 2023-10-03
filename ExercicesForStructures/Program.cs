5ffa914a18387ca78780d99b349307f60d20e16e
unknown <miguelnavarro1567@outlook.com>
2023-10-03 19:41:57 -0300

    For Structures

---
+++ ExercicesForStructures/Program.cs
@@ -0,0 +1,19 @@
+﻿// Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+// X, se for o caso.
+using System;
+
+namespace ExercicesForStructures{
+    class Program{
+        public static void Main(){
+           
+           Console.Write("Digite um numero, para descobrir quais sao seus antecessores impares: ");
+           int numbers = int.Parse(Console.ReadLine());
+           for(int i = 1; i <= numbers; i++){
+            if(i % 2 == 1){
+                Console.Write(i + " ");
+            }
+           }
+
+        }
+    }
+}
