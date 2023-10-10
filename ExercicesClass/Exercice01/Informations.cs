1fa6a007c2a6b32bda5aae916c8ba9eacd5bb0b8
unknown <miguelnavarro1567@outlook.com>
2023-10-10 14:40:37 -0300

    Exercice Class

---
+++ ExercicesClass/Exercice01/Informations.cs
@@ -0,0 +1,7 @@
+namespace Exercice01
+{
+    class Informations{
+        public string Name;
+        public int Age;
+    }
+}
---
+++ ExercicesClass/Exercice01/Program.cs
@@ -0,0 +1,31 @@
+﻿//Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+using System;
+
+namespace Exercice01{
+    class Program{
+        public static void Main(){
+            Informations person1,person2 ;
+            
+            person1 = new Informations();
+            person2 = new Informations();
+
+            Console.Write("Digite o nome da primeira pessoa: ");
+            person1.Name = Console.ReadLine();
+            Console.Write("Digite a idade da primeira pessoa: ");
+            person1.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome da segunda pessoa: ");
+            person2.Name = Console.ReadLine();
+            Console.Write("Digite a idade da segunda pessoa: ");
+            person2.Age = int.Parse(Console.ReadLine());
+
+            if(person1.Age > person2.Age){
+                Console.Write(person1.Name +" é mais velho");
+            }else if (person1.Age < person2.Age){
+                Console.Write(person2.Name +" é mais velho");
+            }else{
+                Console.Write(person1.Name +" e "+ person2.Name+" , possuem a mesma idade.");
+            }
+        }
+    }
+}
